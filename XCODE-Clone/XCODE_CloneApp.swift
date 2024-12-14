3f8c1d11969aadf7f0b16c0c8331b03d9270fe84
Abhee Chaudhary <abheeac.2508@gmail.com>
2024-12-14 19:31:38 +0530

    Initial Commit

---
+++ XCODE-Clone/XCODE_CloneApp.swift
@@ -0,0 +1,32 @@
+//
+//  XCODE_CloneApp.swift
+//  XCODE-Clone
+//
+//  Created by Abhee Chaudhary on 14/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct XCODE_CloneApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ XCODE-CloneTests/XCODE_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XCODE_CloneTests.swift
+//  XCODE-CloneTests
+//
+//  Created by Abhee Chaudhary on 14/12/24.
+//
+
+import Testing
+@testable import XCODE_Clone
+
+struct XCODE_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
