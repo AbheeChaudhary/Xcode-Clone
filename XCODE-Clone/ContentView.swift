702225585f0d2d6f0b71854c738a4a4217336f27
Abhee Chaudhary <abheeac.2508@gmail.com>
2024-12-14 19:48:11 +0530

    This is a commit to see if everything is working or not.

--- XCODE-Clone/ContentView.swift
+++ XCODE-Clone/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  XCODE-Clone
 //
-//  Created by Abhee Chaudhary on 14/12/24.
+//  Created by Abhee Chaudhary on 13/12/24.
 //
 
 import SwiftUI
@@ -13,44 +13,16 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack(spacing : 0){
+            HStack{
+                Text("Run")
+                
             }
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
+    
 }
 
 #Preview {
