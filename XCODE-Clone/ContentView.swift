6cdc8379177f6f2f1cdd739e50394dd981630730
Abhee Chaudhary <abheeac.2508@gmail.com>
2024-12-14 20:00:16 +0530

    Placeholder for topbar created Next -> Adding the buttons

--- XCODE-Clone/ContentView.swift
+++ XCODE-Clone/ContentView.swift
@@ -9,15 +9,35 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    
+    // state variables start
+    
+    @State private var statusMessage: String = "Ready"
+    
+    
+    
+    // State variables end
 
     var body: some View {
         VStack(spacing : 0){
             HStack{
                 Text("Run")
+                    .padding()
+                Spacer()
+                Text("Tool Bar")
+                    .font(.headline)
+                    .padding()
+                Spacer()
+                Text("Settings")
+                    .padding()
+                
                 
             }
+            .frame(height: 40)
+            .background(Color.gray)
         }
     }
 
